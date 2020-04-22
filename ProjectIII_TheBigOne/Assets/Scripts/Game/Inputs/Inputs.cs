efa31f8cf7cb307409e518033ea8a3e012ff9040
Jordi Alba <44704875+jalba002@users.noreply.github.com>
2020-04-22 20:26:45 +0200

    Created input management.

---
+++ ProjectIII_TheBigOne/Assets/Scripts/Game/Inputs/Inputs.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+
+// Scriptable object created for external configuration.
+// If inputs are added or removed, modify the class below.
+namespace Game.Inputs
+{
+    [CreateAssetMenu(fileName = "DATA_Default_Inputs", menuName = "Inputs/Input List")]
+    public class Inputs : ScriptableObject
+    {
+        public InputNames interact;
+        public InputNames running;
+        public InputNames horizontal;
+        public InputNames forward;
+    }
+}

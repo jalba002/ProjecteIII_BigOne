de23064ae9584b4ab91c1bb1e42edc217aa730ab
Jordi Alba <44704875+jalba002@users.noreply.github.com>
2020-04-23 00:05:03 +0200

    Object Inspection

---
+++ ProjectIII_TheBigOne/Assets/Scripts/Game/ObjectInspector.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using Interfaces;
+using UnityEngine;
+
+public class ObjectInspector : MonoBehaviour
+{
+    public IInspectable currentInspectedObject;
+
+    public GameObject feedbackVisual;
+
+    private void Start()
+    {
+        feedbackVisual.SetActive(false);
+    }
+
+    public void Activate()
+    {
+        if (Utils.SimpleRaycast())
+        {
+            
+        }
+    }
+    
+    public bool StartInspect()
+    {
+        currentInspectedObject.Inspect();
+        return false;
+    }
+
+    public void StopInspect()
+    {
+        currentInspectedObject.StopInspect();
+    }
+}
---
+++ ProjectIII_TheBigOne/Assets/Scripts/Interfaces/IInspectable.cs
@@ -0,0 +1,13 @@
+﻿
+
+using UnityEngine;
+
+namespace Interfaces
+{
+    public interface IInspectable
+    {
+        bool IsBeingInspected { get; set; }
+        bool Inspect();
+        bool StopInspect();
+    }
+}

923bc912cb97c614fd9e0f46ad85abde5861f0dd
Jordi Alba <44704875+jalba002@users.noreply.github.com>
2020-04-23 20:03:04 +0200

    Added main enemy requirements.

---
+++ ProjectIII_TheBigOne/Assets/Scripts/Characters/Brains/EnemyBrain.cs
@@ -0,0 +1,11 @@
+﻿using Characters.Brains;
+using UnityEngine;
+
+// This could be the AI brain that makes decisions based on events and stuff.
+public class EnemyBrain : Brain
+{
+    public override void GetActions()
+    {
+        
+    }
+}
---
+++ ProjectIII_TheBigOne/Assets/Scripts/Characters/Enemy/EnemyController.cs
@@ -0,0 +1,89 @@
+﻿using Characters.Brains;
+using Characters.Generic;
+using Player;
+using UnityEngine;
+using CharacterController = Characters.Generic.CharacterController;
+
+namespace Enemy
+{
+    [RequireComponent(typeof(Brain))]
+    [RequireComponent(typeof(State))]
+    [RequireComponent(typeof(StateMachine))]
+    [RequireComponent(typeof(Rigidbody))]
+    public class EnemyController : CharacterController
+    {
+        public Collider attachedCollider;
+        
+        public void Awake()
+        {
+            if (defaultBrain == null)
+            {
+                defaultBrain = GetComponent<Brain>();
+            }
+
+            currentBrain = defaultBrain;
+            
+            if (defaultState == null)
+                defaultState = GetComponent<State>();
+            if (stateMachine == null)
+                stateMachine = GetComponent<StateMachine>();
+            if (rigidbody == null)
+                rigidbody = GetComponent<Rigidbody>();
+
+            if (characterProperties == null)
+            {
+                characterProperties = ScriptableObject.CreateInstance<CharacterProperties>();
+            }
+            else
+            {
+                characterProperties = Instantiate(characterProperties);
+            }
+
+            if (!attachedCollider)
+                attachedCollider = GetComponent<Collider>();
+
+            // audioSource = GetComponent<AudioSource>();
+            /*m_StepTime = Time.time + m_StepTimeRange;
+            m_StepTimeRange = m_StepTimeWaking;*/
+        }
+
+        private void Start()
+        {
+            // enableAirControl = true;
+            stateMachine.SwitchState<State_Enemy_Idle>();
+        }
+
+        void Update()
+        {
+            if (currentBrain.isActiveAndEnabled)
+                currentBrain.GetActions();
+
+            // TODO Disable state machine when game pauses. 
+            // Access with events and disable StateMachine.
+            // DO NOT REFERENCE GAMEMANAGER FROM HERE.
+            if (stateMachine.isActiveAndEnabled)
+            {
+                stateMachine.UpdateTick(Time.deltaTime);
+            }
+        }
+
+        private void FixedUpdate()
+        {
+            // TODO Reenable stop functionality with GameController.
+            // Access with events and disable StateMachine.
+            // DO NOT REFERENCE GAMEMANAGER FROM HERE.
+            if (stateMachine.isActiveAndEnabled) 
+                stateMachine.FixedUpdateTick(Time.fixedDeltaTime);
+        }
+
+        public Transform ReturnSelf()
+        {
+            return this.gameObject.transform;
+        }
+
+        private void OnBecameInvisible()
+        {
+            Debug.LogWarning("I see you.");
+        }
+    }
+}

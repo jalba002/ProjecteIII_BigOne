9bd00a6b4fe338a3d321c26a1ee8b6e487672a98
Jordi Alba <44704875+jalba002@users.noreply.github.com>
2020-04-30 18:57:17 +0200

    Paranormal scares! BOO

---
+++ ProjectIII_TheBigOne/Assets/Scripts/Game/ParanormalManager.cs
@@ -0,0 +1,14 @@
+﻿using Enemy;
+using UnityEngine;
+
+// Also known as the cunt that always tries to scare you.
+public class ParanormalManager : MonoBehaviour
+{
+    private EnemyController Dimitry;
+
+    public void Start()
+    {
+        Dimitry = FindObjectOfType<EnemyController>();
+    }
+
+}
---
+++ ProjectIII_TheBigOne/Assets/Scripts/World/Triggers/Scarejumps/ParanormalTrigger.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using Player;
+using UnityEngine;
+using UnityEngine.Events;
+
+[RequireComponent(typeof(Collider))]
+public class ParanormalTrigger : MonoBehaviour
+{
+    [SerializeField]
+    private bool ActivateOnce = false;
+
+    public Animation[] Animations;
+    public UnityEvent OnActivation;
+
+    private bool hasBeenTriggered = false;
+    private Collider TriggerArea;
+
+    public void Awake()
+    {
+        TriggerArea = GetComponent<Collider>();
+    }
+
+    public void Start()
+    {
+        hasBeenTriggered = false;
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        try
+        {
+            if (other.GetComponent<PlayerController>() != null)
+            {
+                Activate();
+            }
+        }
+        catch (Exception e)
+        {
+            Debug.LogWarning(e.Message);
+        }
+    }
+
+    public void Activate()
+    {
+        if (ActivateOnce)
+        {
+            if (hasBeenTriggered)
+            {
+                Debug.Log("Scare already triggered!");
+                return;
+            }
+        }
+
+        foreach (Animation animation in Animations)
+        {
+            animation.Play();
+        }
+
+        OnActivation.Invoke();
+
+        hasBeenTriggered = true;
+
+        Debug.Log("Trigger activated!");
+    }
+
+    public void Restore()
+    {
+        if (hasBeenTriggered)
+        {
+            Debug.Log("Restoring trigger", this.gameObject);
+            hasBeenTriggered = false;
+        }
+    }
+}

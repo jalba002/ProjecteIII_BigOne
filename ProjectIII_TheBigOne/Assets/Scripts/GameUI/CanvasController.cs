d23df4d6849be2a814cf9906158acee7a2b36583
Oscar G. Subirana <d3cim0@gmail.com>
2020-04-30 03:38:52 +0200

    Basic UI + Message feedback implemented

---
+++ ProjectIII_TheBigOne/Assets/Scripts/GameUI/CanvasController.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CanvasController : MonoBehaviour
+{
+    public GameObject NotificationPrefab;
+    public GameObject NotificationPanel;
+
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.J))
+        {
+            AddPickupMessage("key");
+        }
+    }
+
+    public void AddPickupMessage(string itemName)
+    {
+        GameObject Message = Instantiate(NotificationPrefab, NotificationPanel.transform);
+        //Notifications.Add(Message);
+        //Message.GetComponent<Notification>().SetMessage(string.Format("Picked up {0}", itemName));
+        Message.GetComponent<Notification>().SetMessage(itemName + " COLLECTED");
+    }
+}

864b944d0612f38428c57882f644591d3ed4cc5a
Oscar G. Subirana <d3cim0@gmail.com>
2020-05-01 22:11:59 +0200

    Lighting and Walking UI implemented

--- ProjectIII_TheBigOne/Assets/Scripts/GameUI/CanvasController.cs
+++ ProjectIII_TheBigOne/Assets/Scripts/GameUI/CanvasController.cs
@@ -1,19 +1,42 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using Player;
+using CharacterController = Characters.Generic.CharacterController;
+using UnityEngine.UI;
 
 public class CanvasController : MonoBehaviour
 {
     public GameObject NotificationPrefab;
     public GameObject NotificationPanel;
+    public Slider lightingSlider;
+    public Slider runningSlider;
+    private FlashlightController flashligh;
+    private PlayerController playerController;
+    private State_Player_Walking playerWalking;
+
+
+    private void Start()
+    {
+        flashligh = FindObjectOfType<FlashlightController>();
+        playerController = FindObjectOfType<PlayerController>();
+        playerWalking = FindObjectOfType<State_Player_Walking>();
+    }
 
     void Update()
     {
         if (Input.GetKeyDown(KeyCode.J))
         {
             AddPickupMessage("key");
-            Debug.Log("Hellow");
         }
+
+        playerLightingUpdate();
+        playerRunningUpdate();
+
+        if (playerWalking.currentStamina >= playerController.characterProperties.maximumStamina)
+            runningSlider.gameObject.SetActive(false);
+        else
+            runningSlider.gameObject.SetActive(true);
     }
 
     public void AddPickupMessage(string itemName)
@@ -22,5 +45,15 @@ public class CanvasController : MonoBehaviour
         //Notifications.Add(Message);
         //Message.GetComponent<Notification>().SetMessage(string.Format("Picked up {0}", itemName));
         Message.GetComponent<Notification>().SetMessage(itemName + " COLLECTED");
+    }
+
+    public void playerLightingUpdate()
+    {
+        lightingSlider.value = flashligh.currentCharge / flashligh.maxCharge;
+    }
+
+    public void playerRunningUpdate()
+    {
+        runningSlider.value = playerWalking.currentStamina / playerController.characterProperties.maximumStamina;
     }
 }

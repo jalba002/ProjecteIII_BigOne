1389876aed3070a4f5cd288476c570cc0fd9430d
RkGames <39560269+RkGames@users.noreply.github.com>
2020-05-02 15:06:43 +0200

    Destructible Walls

---
+++ ProjectIII_TheBigOne/Assets/Sandbox/Manel/ExplodeScriptSandbox.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ExplodeScriptSandbox : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public float minForce;
+    public float maxForce;
+    public float radius;
+
+    void Start()
+    {
+        Explode();
+    }
+
+    public void Explode()
+    {
+        foreach (Transform t in transform)
+        {
+            var rb = t.GetComponent<Rigidbody>();
+
+            if(rb != null)
+            {
+                rb.AddExplosionForce(Random.Range(minForce, maxForce), transform.position - t.right * 2, radius);
+            }            
+        }
+        
+    }
+
+
+}
---
+++ ProjectIII_TheBigOne/Assets/Scripts/World/Objects/Destructible_Walls.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Destructible_Walls : MonoBehaviour
+{
+    public GameObject destroyedVersion;
+
+    public float explosionForce;
+    public Vector3 positionToSpawnExplosion;
+    public float radius;
+
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Z))
+        {
+            
+            Instantiate(destroyedVersion, transform.position, transform.rotation);
+            Destroy(gameObject);           
+           
+        }
+    }
+
+}

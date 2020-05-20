2f9251d00818027141a5a24bc517c778960e103e
RkGames <39560269+RkGames@users.noreply.github.com>
2020-05-20 10:08:29 +0200

    DrawerPuzzle Completed

---
+++ ProjectIII_TheBigOne/Assets/Scripts/World/Puzzles/DrawerManager.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DrawerManager : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public DrawerPiece dp1;
+    public DrawerPiece dp2;
+    public DrawerPiece dp3;
+    public DrawerPiece dp4;
+
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void CheckAnswer()
+    {
+        if (dp1.isCorrect() && dp2.isCorrect() && dp3.isCorrect() && dp4.isCorrect())
+        {
+            //UnlockDoor
+            Debug.Log("FUNCIONA");
+        }
+    }
+
+}
---
+++ ProjectIII_TheBigOne/Assets/Scripts/World/Puzzles/DrawerPiece.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DrawerPiece : Puzzle
+{
+    // Start is called before the first frame update
+    public Transform attachedDrawerLocker;
+    public float correctRot;
+    public float currentRot;
+
+    private DrawerManager dm;
+
+    void Start()
+    {
+        dm = GameObject.FindObjectOfType<DrawerManager>();
+        currentRot = transform.eulerAngles.y;
+    }     
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKey(KeyCode.R))
+        {
+            attachedDrawerLocker.eulerAngles += new Vector3(5, 0, 0) * Time.deltaTime;
+        }
+    }
+
+    public void RotateEulerY(float grades = 5)
+    {        
+        currentRot += grades;
+        if (currentRot >= 360)
+            currentRot -= 360;
+        Debug.Log(currentRot);
+        transform.eulerAngles = transform.eulerAngles + new Vector3(0,grades,0);
+        attachedDrawerLocker.eulerAngles = attachedDrawerLocker.eulerAngles + new Vector3(0, grades, 0);
+        dm.CheckAnswer();
+    }
+
+    public bool isCorrect()
+    {
+        if(currentRot == correctRot)
+        {
+            Debug.Log("Correct");
+        }
+        return (currentRot == correctRot);
+    }
+}

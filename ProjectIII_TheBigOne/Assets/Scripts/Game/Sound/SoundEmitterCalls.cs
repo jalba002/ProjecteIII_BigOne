222fed125a79dd8dc4410c4944ccaa8c50bb5807
RkGames <39560269+RkGames@users.noreply.github.com>
2020-06-01 23:30:40 +0200

    Sound on events

---
+++ ProjectIII_TheBigOne/Assets/Scripts/Game/Sound/SoundEmitterCalls.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SoundEmitterCalls : MonoBehaviour
+{
+    public Transform doorPos;
+    public Transform wallPos;
+    public Transform scarejumpPos;
+
+    public string doorSlamPath;
+    public string breakingWallPath;
+    public string scarejumpPath;
+
+    private void Start()
+    {
+        if(doorPos == null)
+        {
+            GameObject.Find("DoorSlamSoundEmitter");
+        }
+        if (wallPos == null)
+        {
+            GameObject.Find("BreakingWallSoundEmitter");
+        }
+        if (scarejumpPos == null)
+        {
+            GameObject.Find("ScarejumpSoundEmitter");
+        }
+    }
+
+
+    public void PlayDoorSlamSound()
+    {
+        SoundManager.Instance.PlaySoundAtLocation(doorSlamPath, doorPos.position, 1, 1, 20);
+    }
+    public void PlayBreakingWallSound()
+    {
+        SoundManager.Instance.PlaySoundAtLocation(breakingWallPath, wallPos.position, 1, 1, 20);
+    }
+    public void PlayScarejumpSound()
+    {
+        SoundManager.Instance.PlaySoundAtLocation(scarejumpPath, scarejumpPos.position, 1, 1, 20);
+    }
+
+
+}

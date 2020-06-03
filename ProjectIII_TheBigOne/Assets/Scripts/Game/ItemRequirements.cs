6e60bbaed0e64e754500b7b3947b70db65ce3aa8
Jordi Alba <44704875+jalba002@users.noreply.github.com>
2020-06-03 12:43:08 +0200

    ReAdded Mats and scene.

--- ProjectIII_TheBigOne/Assets/Scripts/Game/ItemRequirements.cs
+++ ProjectIII_TheBigOne/Assets/Scripts/Game/ItemRequirements.cs
@@ -4,23 +4,25 @@ using World.Objects;
 public class ItemRequirements : MonoBehaviour
 {
     public string requiredItemID;
-    private Inventory playerInventory;
+    private static Inventory playerInventory;
     public DynamicObject lockedDoor;
 
     private void Awake()
     {
-        playerInventory = GameObject.FindObjectOfType<Inventory>();
-        lockedDoor = gameObject.GetComponent<DynamicObject>();
+        if (playerInventory == null)
+            playerInventory = GameObject.FindObjectOfType<Inventory>();
+        
+        lockedDoor = gameObject.GetComponentInChildren<DynamicObject>();
         lockedDoor.OnStartInteracting.AddListener(Unlock);
     }
-    
+
     public void Unlock()
     {
         if (lockedDoor.lockedMode == DynamicObject.LockedMode.Unlocked) return;
-        
+
         var item = playerInventory.characterItems.Find(x => x.itemID == requiredItemID);
         bool hasItem = item != null;
-        
+
         if (hasItem)
         {
             playerInventory.inventoryDisplay.RemoveItem(item);

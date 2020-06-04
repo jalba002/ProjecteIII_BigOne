49a9756072f68cf802d050459208d707cb69a0dc
Jordi Alba <44704875+jalba002@users.noreply.github.com>
2020-06-04 19:23:43 +0200

    UI and Menus

---
+++ ProjectIII_TheBigOne/Assets/Scripts/Game/GameFinisher.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using Player;
+using UnityEngine;
+
+public class GameFinisher : MonoBehaviour
+{
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.GetComponent<PlayerController>() != null)
+        {
+            GameManager.Instance.EndGame();
+        }
+    }
+}

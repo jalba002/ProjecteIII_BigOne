5dcf35c521d8260d5a801a2d0043210294794bf8
Jordi Alba <44704875+jalba002@users.noreply.github.com>
2020-06-18 17:02:14 +0200

    Wall and stuff.

---
+++ ProjectIII_TheBigOne/Assets/TraversableWall.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class TraversableWall : MonoBehaviour
+{
+    [Header("Settings")] public float removalTime;
+
+    [Space(10)] [Header("Components")] public OffMeshLink attachedLink;
+
+    void Start()
+    {
+        if (attachedLink == null)
+            attachedLink = GetComponent<OffMeshLink>();
+    }
+}
